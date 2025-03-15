0b0d75382ba5728537df78cb24461850034aba76
reno <renatrenat132@gmail.com>
2025-03-15 17:49:56 +0200

    Added a helpful comment to manifest.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,6 +6,10 @@
     This is a test app
     ''',
 
+    '''
+    'data' is sequentially loaded, so write down the nondepended files first and then the depended files.
+    '''
+
     'data': [
         'security/ir.model.access.csv',
     ],
