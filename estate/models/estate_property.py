77b9f43d027b48fce001f92a4404853b1c7b00a5
reno <renatrenat132@gmail.com>
2025-03-20 18:24:16 +0200

    Did more tasks from chapter 5

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,6 @@
 from odoo import models, fields
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -7,10 +9,10 @@ class EstateProperty(models.Model):
     name = fields.Char('Estate Property', required = True)
     description = fields.Text('Description of estate')
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(default = lambda self: date.today() + relativedelta(months=3), copy = False)
     expected_price = fields.Float(required = True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly = True, copy = False)
+    bedrooms = fields.Integer(default = 2)
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
