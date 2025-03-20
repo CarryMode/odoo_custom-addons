08e12dcc76edae25b7c89ba44ac911c871636c2e
reno <renatrenat132@gmail.com>
2025-03-20 19:29:35 +0200

    Actually finished chapter 5 with all tasks completed

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -13,15 +13,27 @@ class EstateProperty(models.Model):
     expected_price = fields.Float(required = True)
     selling_price = fields.Float(readonly = True, copy = False)
     bedrooms = fields.Integer(default = 2)
-    garage = fields.Boolean()
+    garage = fields.Boolean(default = True)
     garden = fields.Boolean()
     garden_area = fields.Integer()
+    active = fields.Boolean()
     garden_orientation = fields.Selection([
         ('north','North'),
         ('south','South'),
         ('east','East'),
         ('west','West'),
     ])
+    state = fields.Selection([
+        ('new','New'),
+        ('offer received','Offer Recieved'),
+        ('offer accepted','Offer Accepted'),
+        ('sold','Sold'),
+        ('cancelled','Cancelled'),
+    ],
+    requider = True,
+    copy = False,
+    default = 'new')
+
 
 
 
