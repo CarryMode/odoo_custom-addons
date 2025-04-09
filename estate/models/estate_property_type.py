9a6b155ab9f1a198499782f63cb6c9361b3fdf00
reno <renatrenat132@gmail.com>
2025-04-10 00:09:38 +0300

    Made progress

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -1,8 +1,15 @@
 from odoo import models, fields
-
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = 'Estate Property Type Module'
 
-    name = fields.Char('Name', required = True)
+    name = fields.Char('Title', required = True)
+    description = fields.Text('Description of estate type')
+    postcode = fields.Char()
+    date_availability = fields.Date(default = lambda self: date.today() + relativedelta(months=3), copy = False)
+    available_from = fields.Date()
+    expected_price = fields.Float(required = True)
+    selling_price = fields.Float(readonly = True, copy = False)
